abdd76ae2408c19c548bb0909960bd92c000412a
DDEAL\moussa <m.sow@deal.fr>
2020-08-31 10:20:49 +0200

    refato structure

--- server/controllers/tasks/model.ts
+++
@@ -1,6 +0,0 @@
-export class Task {
-	id?: number;
-	description: string;
-	date: Date;
-	done: boolean;
-}
--- src/app/shared/model/stat.ts
+++
@@ -1,8 +0,0 @@
-import { Task } from '../../../../server/controllers/tasks/model';
-
-export interface Stat {
-	total: number;
-	done: number;
-	undone: number;
-	past: number;
-}
